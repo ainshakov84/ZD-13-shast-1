3c2d24885ac3fb710ff56ddb775525ced3ceb66f
Александр Иншаков <a.inshakov84@gmail.com>
2022-02-08 11:42:26 +0300

    Добавьте файлы проекта.

---
+++ ZD 13 DZ/MyCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ZD_13_DZ
+{
+    internal class MyCommand
+    {
+
+        public static RoutedCommand Exit { get; set; }
+        static MyCommand()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "CTRL+T"));
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(MyCommand), inputs);
+        }
+
+    }
+}
